7227453887353635354c54aef843726442f14cc2
mehdizhd11 <98504734+mehdizhd11@users.noreply.github.com>
2024-09-06 16:56:03 +0330

    final commit

--- elasticdataview/elastic_charts/urls.py
+++ elasticdataview/elastic_charts/urls.py
@@ -5,4 +5,8 @@ from .views import *
 urlpatterns = [
     path('elastic-charts/range-view/<int:pk>/', RangeView.as_view(), name='elastic-charts-range-view'),
     path('elastic-charts/complete-view/<int:pk>/', CompleteView.as_view(), name='elastic-charts-complete-view'),
+    path('elastic-charts/aggregation-view/<int:pk>/', AggregationView.as_view(),
+         name='elastic-charts-aggregation-view'),
+    path('elastic-charts/termquery-view/<int:pk>/', TermQueryView.as_view())
+
 ]
--- elasticdataview/elastic_charts/views.py
+++ elasticdataview/elastic_charts/views.py
@@ -26,14 +26,12 @@ class CompleteView(APIView):
             query_params = request.query_params.dict()
             index = query_params.get("index")
 
-            # Initialize the scroll and set the initial search
             data = es.search(index=index, body=body, scroll='1m', size=10000)
             scroll_id = data['_scroll_id']
 
             response_data = [hit['_source'] for hit in data['hits']['hits']]
 
             while len(data['hits']['hits']) > 0:
-                print(len(data['hits']['hits']))
                 data = es.scroll(scroll_id=scroll_id, scroll='1m')
                 scroll_id = data['_scroll_id']
                 response_data.extend(hit['_source'] for hit in data['hits']['hits'])
@@ -77,11 +75,98 @@ class RangeView(APIView):
                 }
             }
 
-            data = es.search(index=index, body=body)
+            data = es.search(index=index, body=body, scroll='1m', size=10000)
+            scroll_id = data['_scroll_id']
 
             response_data = [hit['_source'] for hit in data['hits']['hits']]
 
-            message = {'status': 'OK', 'data': response_data}
+            while len(data['hits']['hits']) > 0:
+                data = es.scroll(scroll_id=scroll_id, scroll='1m')
+                scroll_id = data['_scroll_id']
+                response_data.extend(hit['_source'] for hit in data['hits']['hits'])
+
+            es.clear_scroll(scroll_id=scroll_id)
+
+            message = {'status': 'OK', "length": len(response_data), 'data': response_data}
+            return Response(message, status=status.HTTP_200_OK)
+        except Exception as e:
+            message = {'status': 'ERROR', 'message': str(e)}
+            return Response(message, status=status.HTTP_400_BAD_REQUEST)
+
+
+class AggregationView(APIView):
+
+    def get(self, request, pk):
+        try:
+            es_config = get_object_or_404(ElasticConfig, pk=pk)
+            es = Elasticsearch(
+                [es_config.host],
+                http_auth=(es_config.username, es_config.password),
+                verify_certs=False,
+            )
+
+            query_params = request.query_params.dict()
+            index = query_params.get("index")
+            field = query_params.get("field")
+
+            body = {
+                "aggs": {
+                    "agg_result": {
+                        "terms": {
+                            "field": field,
+                            "size": 10000,
+                        }
+                    }
+                }
+            }
+
+            data = es.search(index=index, body=body)
+            agg_result = data['aggregations']['agg_result']['value']
+
+            message = {'status': 'OK', 'aggregation_result': agg_result}
+            return Response(message, status=status.HTTP_200_OK)
+        except Exception as e:
+            message = {'status': 'ERROR', 'message': str(e)}
+            return Response(message, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TermQueryView(APIView):
+
+    def get(self, request, pk):
+        try:
+            es_config = get_object_or_404(ElasticConfig, pk=pk)
+            es = Elasticsearch(
+                [es_config.host],
+                http_auth=(es_config.username, es_config.password),
+                verify_certs=False,
+            )
+
+            query_params = request.query_params.dict()
+            index = query_params.get("index")
+            field = query_params.get("field")
+            value = query_params.get("value")
+
+            body = {
+                "query": {
+                    "term": {
+                        field: value
+                    }
+                }
+            }
+
+            data = es.search(index=index, body=body, scroll='1m', size=10000)
+            scroll_id = data['_scroll_id']
+
+            response_data = [hit['_source'] for hit in data['hits']['hits']]
+
+            while len(data['hits']['hits']) > 0:
+                data = es.scroll(scroll_id=scroll_id, scroll='1m')
+                scroll_id = data['_scroll_id']
+                response_data.extend(hit['_source'] for hit in data['hits']['hits'])
+
+            es.clear_scroll(scroll_id=scroll_id)
+
+            message = {'status': 'OK', "length": len(response_data), 'data': response_data}
             return Response(message, status=status.HTTP_200_OK)
         except Exception as e:
             message = {'status': 'ERROR', 'message': str(e)}
